0d7746d2fd7b941646040b9e6d27cbc7eaa5529d
sparxastronomy <sparxastronomy>
2019-12-04 20:04:16 +0530

    typos

--- Basic_FITS_Image.py
+++ Basic_FITS_Image.py
@@ -1,4 +1,4 @@
-import matplotli.pyplot as plt
+import matplotlib.pyplot as plt
 from astropy.io import fits
 import astropy.visualization as viz
 
@@ -8,7 +8,7 @@ hdul.info()
 header_number=int(input("Enter Header number whose data  you want view : "))
 image=hdul[header_number].data
 hdul.close()
-#stretching and normalizing using LogStretch() and MinMaxInterval() like in DS9
+##stretching and normalizing using LogStretch() and MinMaxInterval() like in DS9
 log_param=float(input("Enter base value for logrithmic stretch : "))
 norm=viz.ImageNormalize(image,interval=viz.MinMaxInterval(),stretch=viz.LogStretch())
 plt.imshow(image,cmap='gray',norm=norm)
