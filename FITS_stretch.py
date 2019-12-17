e08cebe7a0a2edbeedbc3540fed1950cc8692d0b
sparxastronomy <sparxastronomy>
2019-12-18 01:32:06 +0530

    added comparitive stretch viewing for single file

--- FITS_stretch.py
+++ FITS_stretch.py
@@ -10,15 +10,27 @@ def log(image_name):
     image=hdul[header_number].data
     hdul.close()
     flag=1
+    total_count=1
+    previous_parameter=0
     while(flag!= 0):
         ##stretching and normalizing using LogStretch() and MinMaxInterval() like in DS9
         log_param=float(input("Enter base value for logrithmic stretch : "))
         norm=viz.ImageNormalize(image,interval=viz.MinMaxInterval(),stretch=viz.LogStretch(log_param))
-        plt.imshow(image,cmap='gray',norm=norm)
+        if total_count>1:
+            fig,(ax1,ax2) = plt.subplots(1, 2)
+            norm=viz.ImageNormalize(image,interval=viz.MinMaxInterval(),stretch=viz.LogStretch(log_param))
+            ax1.imshow(image,cmap='gray',norm=norm)
+            log_param=previous_parameter
+            norm=viz.ImageNormalize(image,interval=viz.MinMaxInterval(),stretch=viz.LogStretch(log_param))
+            ax2.imshow(image,cmap='gray',norm=norm)
+        else:
+            plt.imshow(image,cmap='gray',norm=norm)
         plt.show()
         ch=input("Are you happy with your choice of log_parameters(Y/N) : ")
         if ch=='Y' or ch=='y':
             flag=0
             return norm(image)
         else:
-            flag=1   
+            flag=1 
+            total_count+=1
+            previous_parameter=log_param  
